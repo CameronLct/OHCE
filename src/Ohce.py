d86334b8d8c1a5725647381c6a40f0a434984379
Cameron Leconte <cameron.leconte@aquilapp.com>
2023-01-31 13:53:18 +0100

    ajout des différentes langues

--- src/Ohce.py
+++ src/Ohce.py
@@ -1,12 +1,14 @@
 class Ohce:
+    def __init__(self, langue):
+        self.langue = langue
     def bien_dit(self):
-        return "Bien dit"
+        return self.langue.bien_dit()
 
-    def bonjour(self, ):
-        return "Bonjour"
+    def bonjour(self):
+        return self.langue.bonjour()
 
     def au_revoir(self):
-        return "Au revoir"
+        return self.langue.au_revoir()
 
     def miroir(self, chaine):
         return chaine[::-1]
