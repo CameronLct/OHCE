7a2b2527874550c1620f0119e4d193b172b0b8bb
Cameron Leconte <cameron.leconte@aquilapp.com>
2023-01-31 20:48:19 +0100

    changement avec les constantes

--- test/DefaultTest.py
+++ test/DefaultTest.py
@@ -24,15 +24,6 @@ class PalindromeTest(unittest.TestCase):
 
         resultat = ohce.palindrome(palindrome)
 
-        self.assertIn(bonjour + palindrome + bien_dit + au_revoir, resultat)
-
-
-        langue_spy = LangueSpy()
-        ohce = OhceBuilder().langue_defini(langue_spy).build()
-
-        ohce.palindrome("Cameron")
-
-        self.assertEqual(0, langue_spy.nb_bien_dit)
 
         locale_language, locale_encoding = locale.getlocale()
         current_time = datetime.datetime.now().time()
@@ -42,28 +33,28 @@ class PalindromeTest(unittest.TestCase):
             langue_locale = LangueFr()
             if current_time >= datetime.time(6) and current_time < datetime.time(18):
                 periode_locale = Periode.MATIN
-                bonjour_locale = "Bonjour"
+                bonjour_locale = Constantes.Francais.BONJOUR
             else:
                 periode_locale = Periode.NUIT
-                bonjour_locale = "Bonsoir"
-            au_revoir_locale = "Au revoir"
-            bien_dit_locale = "Bien dit"
+                bonjour_locale = Constantes.Francais.BONSOIR
+            au_revoir_locale = Constantes.Francais.AU_REVOIR
+            bien_dit_locale = Constantes.Francais.BIEN_DIT
         else:
             langue_locale = LangueEn()
             if current_time >= datetime.time(6) and current_time < datetime.time(12):
                 periode_locale = Periode.MATIN
-                bonjour_locale = "Good Morning"
+                bonjour_locale = Constantes.English.GOOD_MORNING
             elif current_time >= datetime.time(12) and current_time < datetime.time(18):
                 periode_locale = Periode.APRES_MIDI
-                bonjour_locale = "Good Afternoon"
+                bonjour_locale = Constantes.English.GOOD_AFTERNOON
             elif current_time >= datetime.time(18) and current_time < datetime.time(22):
                 periode_locale = Periode.SOIR
-                bonjour_locale = "Good Evening"
+                bonjour_locale = Constantes.English.GOOD_EVENING
             else:
                 periode_locale = Periode.NUIT
-                bonjour_locale = "Good Night"
-            au_revoir_locale = "Good bye"
-            bien_dit_locale = "Well done"
+                bonjour_locale = Constantes.English.GOOD_NIGHT
+            au_revoir_locale = Constantes.English.GOOD_BYE
+            bien_dit_locale = Constantes.English.WELL_DONE
 
         ohcebuild = OhceBuilder().langue_defini(langue_locale).periode_defini(periode_locale).build()
 
